652da645118d9c310e7021466b0c172a52a53423
Laure QUINTRIC <lquintri@datarmor2.ib0.ice.ifremer.fr>
2019-10-17 14:37:11 +0000

    update RunNextMB.sh

--- RunNextMB.sh
+++ RunNextMB.sh
@@ -2,13 +2,10 @@
 #activate nextflow environment
 . /appli/bioinfo/nextflow/19.07.0/env.sh
 
-TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
-
 #nextflow temp directory
 export NXF_TEMP=$SCRATCH
 
-#run nextflow nextmb workflow
-#nextflow run MB.nf $1 > $TIMESTAMP-nextflow.log 2>&1
+#run nextflow nextmb workflow ($1 is useful if you want to run resume)
 nextflow -trace nextflow.executor run MB.nf $1
 
 #deactivate nextflow environment

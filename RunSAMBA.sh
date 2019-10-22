cf2aec0bf4d064491d6c9afcbc42573bc2573249
Laure QUINTRIC <lquintri@datarmor2.ib0.ice.ifremer.fr>
2019-10-22 15:17:35 +0000

    add trace for cluster executor

--- RunSAMBA.sh
+++ RunSAMBA.sh
@@ -6,8 +6,7 @@
 export NXF_TEMP=$SCRATCH
 
 #run nextflow nextmb workflow ($1 is useful if you want to run resume)
-#nextflow -trace nextflow.executor run MB.nf $1
-nextflow run SAMBA.nf $1
+nextflow -trace nextflow.executor run MB.nf $1
 
 #deactivate nextflow environment
 . /appli/bioinfo/nextflow/19.07.0/delenv.sh

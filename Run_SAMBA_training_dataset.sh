18e94f6f3e491055d5326d37f0f0bf9410d639e0
NOEL <cyril.noel@ifremer.fr>
2019-10-28 14:55:35 +0100

    Add Run_SAMBA_training_dataset.sh to run only the training dataset

---
+++ Run_SAMBA_training_dataset.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+#activate nextflow environment
+. $BASEDIR/config/conda_envs/nextflow_env.sh
+
+#nextflow temp directory
+export NXF_TEMP=$SCRATCH
+
+#modify path to the training dataset data
+sed -i "s/PATH\/TO/${BASEDIR}/g" $BASEDIR/training_dataset/q2_manifest
+
+#run nextflow nextmb workflow ($1 is useful if you want to run resume)
+nextflow -trace nextflow.executor run MB.nf $1
+
+#deactivate nextflow environment
+. $BASEDIR/config/conda_envs/delenv.sh

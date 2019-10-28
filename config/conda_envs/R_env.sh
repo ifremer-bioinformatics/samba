f68a6f6179985c6e8f13ad72779effa07182a2fc
NOEL <cyril.noel@ifremer.fr>
2019-10-28 14:36:48 +0100

    Add R_env.sh to load R conda environment

---
+++ config/conda_envs/R_env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+export PATH=/appli/bioinfo/R/3.6.1/bin/:$PATH
+export PKG_CONFIG_PATH=/appli/conda-env/bioinfo/r-3.6.1/lib/pkgconfig
+export PATH=/appli/anaconda/2.7/bin/:$PATH
+source activate /appli/conda-env/bioinfo/r-3.6.1

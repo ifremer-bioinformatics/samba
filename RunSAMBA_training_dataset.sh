133b89c809411d1f44bdcd374fa8544f78a3adb5
NOEL <cyril.noel@ifremer.fr>
2019-10-28 15:16:35 +0100

    Update RunSAMBA_training_dataset.sh

--- RunSAMBA_training_dataset.sh
+++ RunSAMBA_training_dataset.sh
@@ -9,7 +9,7 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 export NXF_TEMP=$SCRATCH
 
 #modify path to the training dataset data
-sed -i "s/PATH\/TO/${BASEDIR}/g" $BASEDIR/training_dataset/q2_manifest
+sed -i "s|PATH/TO|$BASEDIR|g" $BASEDIR/training_dataset/q2_manifest
 
 #run nextflow nextmb workflow ($1 is useful if you want to run resume)
 nextflow -trace nextflow.executor run MB.nf $1

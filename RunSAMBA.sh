ba467922dc3c3201e78c51e73aea3679f0ddad47
NOEL <cyril.noel@ifremer.fr>
2019-10-29 16:44:33 +0100

    Update RunSAMBA.sh

--- RunSAMBA.sh
+++ RunSAMBA.sh
@@ -6,7 +6,21 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 . $BASEDIR/config/conda_envs/nextflow_env.sh
 
 #nextflow temp directory
-export NXF_TEMP=$SCRATCH
+if [ ! -z $TMP ] 
+then 
+  sed -i 's|/PATH/TO/OUTDIR/$projectName|$TMP/output.test/$projectName|g' config/params.config
+  #nextflow temp directory
+  export NXF_TEMP=$TMP
+elseif [ ! -z $SCRATCH ] 
+then
+  sed -i 's|/PATH/TO/OUTDIR/$projectName|$SCRATCH/output.test/$projectName|g' config/params.config
+  #nextflow temp directory
+  export NXF_TEMP=$SCRATCH
+else
+  sed -i 's|/PATH/TO/OUTDIR/$projectName|${baseDir}/output.test/$projectName|g' config/params.config
+  #nextflow temp directory
+  export NXF_TEMP=$BASEDIR
+fi
 
 #run nextflow nextmb workflow ($1 is useful if you want to run resume)
 nextflow -trace nextflow.executor run SAMBA.nf $1

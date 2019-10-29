d826fd83d2c02f73fbe9121066a77f7365a81fd6
Cyril NOEL <cyril.noel@ifremer.fr>
2019-10-29 07:51:55 +0000

    update path to outdir

--- RunSAMBA_training_dataset.sh
+++ RunSAMBA_training_dataset.sh
@@ -16,15 +16,17 @@ if [ ! -z $TMP ]
 then 
   sed -i 's|/PATH/TO/OUTDIR/$projectName|$TMP/output.test/$projectName|g' config/params.config
   mkdir -p $TMP/tax.databases.test/
+  export tax_db_dir=$TMP/tax.databases.test/
   sed -i "s|/PATH/TO/qiime2/2019.07/DATABASE|$TMP/tax.databases.test/DATABASE_silva_v132_99_16S.qza|g" config/params.config
 else 
-  sed -i 's|/PATH/TO/OUTDIR/$projectName|$BASEDIR/output.test/$projectName|g' config/params.config
+  sed -i 's|/PATH/TO/OUTDIR/$projectName|${basedir}/output.test/$projectName|g' config/params.config
   mkdir -p $BASEDIR/tax.databases.test
+  export tax_db_dir=$BASEDIR/tax.databases.test/
   sed -i "s|/PATH/TO/qiime2/2019.07/DATABASE|$BASEDIR/tax.databases.test/DATABASE_silva_v132_99_16S.qza|g" config/params.config
 fi
 
 #download taxonomic database
-wget ftp://ftp.ifremer.fr/ifremer/dataref/bioinfo/sebimer/sequence-set/qiime2/2019.07/DATABASE_silva_v132_99_16S.qza -O $tax.db.test
+wget ftp://ftp.ifremer.fr/ifremer/dataref/bioinfo/sebimer/sequence-set/qiime2/2019.07/DATABASE_silva_v132_99_16S.qza -O $tax_db_dir/DATABASE_silva_v132_99_16S.qza 
 
 #run nextflow nextmb workflow ($1 is useful if you want to run resume)
 nextflow -trace nextflow.executor run SAMBA.nf $1

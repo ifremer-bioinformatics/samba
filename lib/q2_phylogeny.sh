892efe64c9261cdcab8773c045b82d337be8a6ee
Cyril NOEL <cyril.noel@ifremer.fr>
2019-12-04 09:35:33 +0000

    add threads automatic detection

--- lib/q2_phylogeny.sh
+++ lib/q2_phylogeny.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ## Command run by snakemake :
-### ${baseDir}/lib/q2_phylogeny.sh {repseq} {output.aligned} {output.masked} {output.tree} {params.model} {params.alrt} {params.bootstrap} {output.tree_bestmodel} {params.tree_log} {ressources.ncpus}
+### ${baseDir}/lib/q2_phylogeny.sh repseqs aligned_repseq masked_aligned tree model ${params.phylogeny.alrt} ${params.phylogeny.bootstrap} tree_bestmodel tree_log completecmd > q2_phylogeny.log 2>&1
 
 # Arguments
 args=("$@")
@@ -13,7 +13,7 @@ alrt=${args[5]}
 bootstrap=${args[6]}
 tree_bestmodel=${args[7]}
 tree_log=${args[8]}
-ncpus=${args[9]}
+logcmd=${args[9]}
 
 # Alignment of representative sequences
 cmd="qiime alignment mafft --i-sequences $repseq --o-alignment $aligned"
@@ -26,13 +26,13 @@ echo $cmd >> $logcmd
 eval $cmd
 
 # IQ-tree phylogeny
-cmd="qiime phylogeny iqtree-ultrafast-bootstrap --i-alignment $masked --p-n-cores $ncpus --o-tree $tree --verbose >& $model 2>&1"
+cmd="qiime phylogeny iqtree-ultrafast-bootstrap --i-alignment $masked --p-n-cores 0 --o-tree $tree --verbose >& $model 2>&1"
 echo $cmd >> $logcmd
 eval $cmd
 
 best_model="$(grep 'Best-fit model' $model_log | cut -d ' ' -f3)"
 
-cmd="qiime phylogeny iqtree-ultrafast-bootstrap --i-alignment $masked --p-n-cores $ncpus --p-alrt $alrt --p-abayes --p-lbp $bootstrap --p-substitution-model $best_model --o-tree $tree_bestmodel --verbose >& $tree_log 2>&1"
+cmd="qiime phylogeny iqtree-ultrafast-bootstrap --i-alignment $masked --p-n-cores 0 --p-alrt $alrt --p-abayes --p-lbp $bootstrap --p-substitution-model $best_model --o-tree $tree_bestmodel --verbose >& $tree_log 2>&1"
 echo $cmd >> $logcmd
 eval $cmd
 

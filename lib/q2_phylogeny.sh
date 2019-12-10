c9ed8ea2e45124c2c70c2b0cd9412d290e518b83
Cyril NOEL <cyril.noel@ifremer.fr>
2019-12-10 15:14:19 +0000

    change method for phylogeny contruction

--- lib/q2_phylogeny.sh
+++ lib/q2_phylogeny.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ## Command run by snakemake :
-### ${baseDir}/lib/q2_phylogeny.sh repseqs aligned_repseq masked_aligned tree model ${params.phylogeny.alrt} ${params.phylogeny.bootstrap} tree_bestmodel tree_log completecmd > q2_phylogeny.log 2>&1
+### ${baseDir}/lib/q2_phylogeny.sh ${repseqs_phylo} aligned_repseq.qza masked-aligned_repseq.qza tree.qza tree.log tree_export_dir tree_export.log completecmd > q2_phylogeny.log 2>&1
 
 # Arguments
 args=("$@")
@@ -8,12 +8,10 @@ repseq=${args[0]}
 aligned=${args[1]}
 masked=${args[2]}
 tree=${args[3]}
-model=${args[4]}
-alrt=${args[5]}
-bootstrap=${args[6]}
-tree_bestmodel=${args[7]}
-tree_log=${args[8]}
-logcmd=${args[9]}
+tree_log=${args[4]}
+export=${args[5]}
+tree_export=${args[6]}
+logcmd=${args[7]}
 
 # Alignment of representative sequences
 cmd="qiime alignment mafft --i-sequences $repseq --o-alignment $aligned"
@@ -25,14 +23,13 @@ cmd="qiime alignment mask --i-alignment $aligned --o-masked-alignment $masked"
 echo $cmd >> $logcmd
 eval $cmd
 
-# IQ-tree phylogeny
-cmd="qiime phylogeny iqtree-ultrafast-bootstrap --i-alignment $masked --p-n-cores 0 --o-tree $tree --verbose >& $model 2>&1"
+# fasttree phylogeny
+cmd="qiime phylogeny fasttree --i-alignment $masked --o-tree $tree >& $tree_log 2>&1"
 echo $cmd >> $logcmd
 eval $cmd
 
-best_model="$(grep 'Best-fit model' $model_log | cut -d ' ' -f3)"
-
-cmd="qiime phylogeny iqtree-ultrafast-bootstrap --i-alignment $masked --p-n-cores 0 --p-alrt $alrt --p-abayes --p-lbp $bootstrap --p-substitution-model $best_model --o-tree $tree_bestmodel --verbose >& $tree_log 2>&1"
+# tree export
+cmd="qiime tools export --input-path $tree --output-path $export >& $tree_export 2>&1"
 echo $cmd >> $logcmd
 eval $cmd
 

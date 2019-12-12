d4eb24793705b65d25d89f31834f95ed65dfbb93
Cyril NOEL <cyril.noel@ifremer.fr>
2019-12-12 08:40:29 +0000

    add MDS-PCoA, Hierarchical clustering for multiple distance indexes

--- lib/beta_diversity.R
+++ lib/beta_diversity.R
@@ -12,7 +12,7 @@
 ##          SeBiMER, Ifremer                                                 ##
 ##                                                                           ##
 ## Creation Date: 2019-08-29                                               ####
-## Modified on: 2019-10-23                                                 ####
+## Modified on: 2019-12-11                                                 ####
 ##                                                                           ##
 ## Copyright (c) SeBiMER, august-2019                                      ####
 ## Emails: cyril.noel@ifremer.fr and laure.quintric@ifremer.fr             ####
@@ -35,46 +35,152 @@ library("tidyr")
 library("gridExtra")
 library("egg")
 library("vegan")
+library("dendextend")
 
-betadiversity <- function(PHYLOSEQ, distance, criteria, samples_ordination_plot, metadata) {
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
+#										#
+# Function to standardize ordination analysis and plot for each distance matrix #
+#										#
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
 
-    #### @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ####
-    ## ** Beginning of the script: analysis of the beta diversity **           ####
+ 
+betadiversity <- function(PHYLOSEQ, distance, metadata, criteria, nmds, pcoa, method_hc, plot_hc) {
     
     #~~~~~~~~~~~~~~~~~~~~~#
     # Non-normalized data #
     #~~~~~~~~~~~~~~~~~~~~~#
     
-    ## /1\ Ordination process ####
+    ## Ordination process ####
     metadata = read.table(metadata, row.names=1, h=T, sep="\t", check.names=FALSE)
-    ord = ordinate(PHYLOSEQ,"NMDS",distance,trymax = 1000)
+    
+    if (distance == "jaccard" | distance == "bray") {
+    ord_nmds = ordinate(PHYLOSEQ,"NMDS",distance,trymax = 1000)
+    ord_pcoa = ordinate(PHYLOSEQ,"PCoA",distance,trymax = 1000)
+    }
+    else {
+    ord_nmds = ordinate(PHYLOSEQ,"NMDS",distance)
+    ord_pcoa = ordinate(PHYLOSEQ,"PCoA",distance)
+    }
 
     color_vector = unlist(mapply(brewer.pal, brewer.pal.info[brewer.pal.info$category == 'qual',]$maxcolors, rownames(brewer.pal.info[brewer.pal.info$category == 'qual',])))
     color_samples = color_vector[1:length(levels(metadata[,criteria]))]
 
     group = get_variable(PHYLOSEQ, criteria)
-    anosim_result = anosim(distance(PHYLOSEQ,"bray"),group, permutations = 999)
+    anosim_result = anosim(distance(PHYLOSEQ,distance),group, permutations = 999)
+
+    ## Sample analysis ####
+    ### PHYLOSEQ_OBJ, Ordination, variable to test, colors to use, anosim result, ordination plot name, width of graph, heigth of graph
+    nmds(PHYLOSEQ, ord_nmds, criteria, color_samples, anosim_result, nmds, 12, 10, paste("NMDS on non-normalized data","based on",distance,"distance",sep=" "))
+    mds_pcoa(PHYLOSEQ, ord_pcoa, criteria, color_samples, anosim_result, pcoa, 12, 10, paste("MDS-PCoA on non-normalized data","based on",distance,"distance",sep=" "))
     
-    ## /3\ Sample analysis ####
-    ### PHYLOSEQ_OBJ, Ordination, variable to test, colors to use, anosim result, sample ordination plot name, width of graph, heigth of graph
-    sample_nmds(PHYLOSEQ, ord, criteria, color_samples, anosim_result, samples_ordination_plot, 12, 10, "NMDS on non-normalized data")
+    ## Hierarchical clsutering ####    
+    dist = distance(PHYLOSEQ, distance, type="samples")
+    hc = hclust(dist, method = method_hc)
+    dendro = as.dendrogram(hc)
+    group = data.frame(PHYLOSEQ@sam_data[,criteria])[,1]
+    n_group = length(unique(group))
+    cols = color_vector[1:n_group]
+    col_group = cols[group]
+    plot.hc(dendro, group, cols, col_group, method_hc, distance, plot_hc, 12, 10)
 }
 
-main  <- function(){
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
+#                                             #
+# Ordination process for each distance matrix #
+#                                             #
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
+
+main_jaccard  <- function(){
     # Get arguments from RScript command line
     args = commandArgs(trailingOnly=TRUE)
     PHYLOSEQ = readRDS(args[1])
-    distance = args[2]
+    distance = "jaccard"
     # get criteria and replace "-" character by "_"
-    criteria = str_replace(args[3], "-", "_")
-    samples_ordination_plot = args[4]
-    metadata = args[5]
-    workflow_dir = args[6]
-    if (!exists("sample_nmds", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
-    betadiversity(PHYLOSEQ, distance, criteria, samples_ordination_plot, metadata)
+    criteria = str_replace(args[2], "-", "_")
+    metadata = args[3]
+    workflow_dir = args[4]
+    nmds = args[5]
+    pcoa = args[9]
+    method_hc = args[13]
+    plot_hc = args[14]
+    if (!exists("nmds", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    if (!exists("mds_pcoa", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    if (!exists("plot.hc", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    betadiversity(PHYLOSEQ, distance, metadata, criteria, nmds, pcoa, method_hc, plot_hc)
 }
 
 if (!interactive()) {
-        main()
+        main_jaccard()
+}
+
+
+main_bray  <- function(){
+    # Get arguments from RScript command line
+    args = commandArgs(trailingOnly=TRUE)
+    PHYLOSEQ = readRDS(args[1])
+    distance = "bray"
+    # get criteria and replace "-" character by "_"
+    criteria = str_replace(args[2], "-", "_")
+    metadata = args[3]
+    workflow_dir = args[4]
+    nmds = args[6]
+    pcoa = args[10]
+    method_hc = args[13]
+    plot_hc = args[15]
+    if (!exists("nmds", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    if (!exists("mds_pcoa", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    if (!exists("plot.hc", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    betadiversity(PHYLOSEQ, distance, metadata, criteria, nmds, pcoa, method_hc, plot_hc)
+}
+
+if (!interactive()) {
+        main_bray()
+}
+
+main_unifrac  <- function(){
+    # Get arguments from RScript command line
+    args = commandArgs(trailingOnly=TRUE)
+    PHYLOSEQ = readRDS(args[1])
+    distance = "unifrac"
+    # get criteria and replace "-" character by "_"
+    criteria = str_replace(args[2], "-", "_")
+    metadata = args[3]
+    workflow_dir = args[4]
+    nmds = args[7]
+    pcoa = args[11]
+    method_hc = args[13]
+    plot_hc = args[16]
+    if (!exists("nmds", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    if (!exists("mds_pcoa", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    if (!exists("plot.hc", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    betadiversity(PHYLOSEQ, distance, metadata, criteria, nmds, pcoa, method_hc, plot_hc)
+}
+
+if (!interactive()) {
+        main_unifrac()
+}
+
+main_wunifrac  <- function(){
+    # Get arguments from RScript command line
+    args = commandArgs(trailingOnly=TRUE)
+    PHYLOSEQ = readRDS(args[1])
+    distance = "wunifrac"
+    # get criteria and replace "-" character by "_"
+    criteria = str_replace(args[2], "-", "_")
+    metadata = args[3]
+    workflow_dir = args[4]
+    nmds = args[8]
+    pcoa = args[12]
+    method_hc = args[13]
+    plot_hc = args[17]
+    if (!exists("nmds", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    if (!exists("mds_pcoa", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    if (!exists("plot.hc", mode="function")) source(gsub(" ", "", paste(workflow_dir,"/lib/beta_diversity_graphs.R")))
+    betadiversity(PHYLOSEQ, distance, metadata, criteria, nmds, pcoa, method_hc, plot_hc)
+}
+
+
+if (!interactive()) {
+        main_wunifrac()
 }
 

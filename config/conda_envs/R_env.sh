b6588d991fb7e85550eea4b2ef947aa0b6324494
Cyril NOEL <cyril.noel@ifremer.fr>
2019-12-12 15:47:13 +0000

    update env.sh for R

--- config/conda_envs/R_env.sh
+++ config/conda_envs/R_env.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-export PATH=/appli/bioinfo/R/3.6.1/bin/:$PATH
-export PKG_CONFIG_PATH=/appli/conda-env/bioinfo/r-3.6.1/lib/pkgconfig
-export PATH=/appli/anaconda/2.7/bin/:$PATH
+export PATH=/appli/anaconda/latest/bin/:$PATH
 source activate /appli/conda-env/bioinfo/r-3.6.1

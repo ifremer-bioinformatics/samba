f15c467a18a5d55669694f3068c251e5bdd2cd06
NOEL <cyril.noel@ifremer.fr>
2020-01-10 20:40:11 +0100

    correction of the 'resume' problem

--- RunSAMBA_training_dataset.sh
+++ RunSAMBA_training_dataset.sh
@@ -5,36 +5,44 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 #activate nextflow environment
 . $BASEDIR/config/conda_envs/nextflow_env.sh
 
-#set test directories
-if [ ! -z $TMP ] 
-then 
-  sed -i 's|/PATH-TO/$projectName|$TMP/SAMBA_results_of_${projectName}|g' config/params.config
-  export tax_db_dir=$TMP/tax.databases.test
-  sed -i "s|/PATH-TO/database.qza|$TMP/tax.databases.test/DATABASE_silva_v132_99_16S.qza|g" config/params.config
-  #nextflow temp directory
-  export NXF_TEMP=$TMP
-elif [ ! -z $SCRATCH ] 
-then
-  sed -i 's|/PATH-TO/$projectName|$SCRATCH/SAMBA_results_of_${projectName}|g' config/params.config
-  export tax_db_dir=$SCRATCH/tax.databases.test
-  sed -i "s|/PATH-TO/database.qza|$SCRATCH/tax.databases.test/DATABASE_silva_v132_99_16S.qza|g" config/params.config
-  #nextflow temp directory
-  export NXF_TEMP=$SCRATCH
-else
-  sed -i 's|/PATH-TO/$projectName|${baseDir}/SAMBA_results_of_${projectName}|g' config/params.config
-  export tax_db_dir=$BASEDIR/tax.databases.test
-  sed -i "s|/PATH-TO/database.qza|$BASEDIR/tax.databases.test/DATABASE_silva_v132_99_16S.qza|g" config/params.config
-  #nextflow temp directory
-  export NXF_TEMP=$BASEDIR
-fi
-
-#download taxonomic database
-DB=$tax_db_dir/DATABASE_silva_v132_99_16S.qza
-if [ -f "$DB" ]; then
+if [ "$1" != "-resume" ]
+  
+  #set test directories
+  if [ ! -z $SCRATCH ] 
+  then 
+    sed -i 's|/PATH-TO/$projectName|$SCRATCH/SAMBA_results_of_${projectName}|g' config/params.config
+    sed -i 's|/PATH-TO|$BASEDIR|g' $BASEDIR/training_dataset/q2_manifest
+    #nextflow temp directory
+    export NXF_TEMP=$SCRATCH
+    export tax_db_dir=$SCRATCH/tax.databases.test
+    sed -i 's|/PATH-TO/database.qza|$SCRATCH/tax.databases.test/DATABASE_silva_v132_99_16S.qza|g' config/params.config
+  elif [  ! -z $TMP  ] 
+  then
+    sed -i 's|/PATH-TO/$projectName|$TMP/SAMBA_results_of_${projectName}|g' config/params.config
+    sed -i 's|/PATH-TO|$BASEDIR|g' $BASEDIR/training_dataset/q2_manifest 
+    #nextflow temp directory
+    export NXF_TEMP=$TMP
+    export tax_db_dir=$TMP/tax.databases.test
+    sed -i 's|/PATH-TO/database.qza|$TMP/tax.databases.test/DATABASE_silva_v132_99_16S.qza|g' config/params.config
+  else
+    sed -i 's|/PATH-TO/$projectName|${baseDir}/SAMBA_results_of_${projectName}|g' config/params.config
+    sed -i 's|/PATH-TO|$BASEDIR|g' $BASEDIR/training_dataset/q2_manifest 
+    #nextflow temp directory
+    export NXF_TEMP=$BASEDIR
+    export tax_db_dir=$BASEDIR/tax.databases.test
+    sed -i 's|/PATH-TO/database.qza|$BASEDIR/tax.databases.test/DATABASE_silva_v132_99_16S.qza|g' config/params.config
+  fi
+  
+  #download taxonomic database
+  DB=$tax_db_dir/DATABASE_silva_v132_99_16S.qza
+  if [ -f "$DB" ]
+  then
     echo "$DB exist"
-else
+  else
     mkdir -p $tax_db_dir
-    wget ftp://ftp.ifremer.fr/ifremer/dataref/bioinfo/sebimer/sequence-set/qiime2/2019.07/DATABASE_silva_v132_99_16S.qza -O $tax_db_dir/DATABASE_silva_v132_99_16S.qza
+    wget ftp://ftp.ifremer.fr/ifremer/dataref/bioinfo/sebimer/sequence-set/qiime2/2019.10/SILVA_v132/DATABASE_silva_v132_99_16S.qza -O $tax_db_dir/DATABASE_silva_v132_99_16S.qza
+  fi
+
 fi
 
 #run nextflow nextmb workflow ($1 is useful if you want to run resume)

a42458bb61cf62eccdf8e738f9c7b6d4a8245c77
NOEL <cyril.noel@ifremer.fr>
2020-01-10 21:58:03 +0100

    Update RunSAMBA_training_dataset.sh

--- RunSAMBA_training_dataset.sh
+++ RunSAMBA_training_dataset.sh
@@ -6,6 +6,7 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 . $BASEDIR/config/conda_envs/nextflow_env.sh
 
 if [ "$1" != "-resume" ]
+then
   #set test directories
   if [ ! -z $SCRATCH ] 
   then 
@@ -31,7 +32,6 @@ if [ "$1" != "-resume" ]
     export tax_db_dir=$BASEDIR/tax.databases.test
     sed -i 's|/PATH-TO/database.qza|$BASEDIR/tax.databases.test/DATABASE_silva_v132_99_16S.qza|g' config/params.config
   fi
-  
   #download taxonomic database
   DB=$tax_db_dir/DATABASE_silva_v132_99_16S.qza
   if [ -f "$DB" ]
@@ -41,6 +41,8 @@ if [ "$1" != "-resume" ]
     mkdir -p $tax_db_dir
     wget ftp://ftp.ifremer.fr/ifremer/dataref/bioinfo/sebimer/sequence-set/qiime2/2019.10/SILVA_v132/DATABASE_silva_v132_99_16S.qza -O $tax_db_dir/DATABASE_silva_v132_99_16S.qza
   fi
+else
+  echo "Resume of the previous analysis"
 fi
 
 #run nextflow nextmb workflow ($1 is useful if you want to run resume)

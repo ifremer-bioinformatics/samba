3dc92b6a296a7bad9345dadaa0da9a8523b2386a
NOEL <cyril.noel@ifremer.fr>
2020-01-10 20:21:11 +0100

    correction of the 'resume' problem

--- RunSAMBA.sh
+++ RunSAMBA.sh
@@ -5,21 +5,24 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 #activate nextflow environment
 . $BASEDIR/config/conda_envs/nextflow_env.sh
 
-#nextflow temp directory
-if [ ! -z $TMP ] 
-then 
-  sed -i 's|/PATH-TO/$projectName|$TMP/SAMBA_results_of_${projectName}|g' config/params.config
-  #nextflow temp directory
-  export NXF_TEMP=$TMP
-elif [ ! -z $SCRATCH ] 
+if [ "$1" != "-resume" ]
 then
-  sed -i 's|/PATH-TO/$projectName|$SCRATCH/SAMBA_results_of_${projectName}|g' config/params.config
   #nextflow temp directory
-  export NXF_TEMP=$SCRATCH
-else
-  sed -i 's|/PATH-TO/$projectName|${baseDir}/SAMBA_results_of_${projectName}|g' config/params.config
-  #nextflow temp directory
-  export NXF_TEMP=$BASEDIR
+  if [ ! -z $TMP ] 
+  then 
+    sed -i 's|/PATH-TO/$projectName|$TMP/SAMBA_results_of_${projectName}|g' config/params.config
+    #nextflow temp directory
+    export NXF_TEMP=$TMP
+  elif [ ! -z $SCRATCH ] 
+  then
+    sed -i 's|/PATH-TO/$projectName|$SCRATCH/SAMBA_results_of_${projectName}|g' config/params.config
+    #nextflow temp directory
+    export NXF_TEMP=$SCRATCH
+  else
+    sed -i 's|/PATH-TO/$projectName|${baseDir}/SAMBA_results_of_${projectName}|g' config/params.config
+    #nextflow temp directory
+    export NXF_TEMP=$BASEDIR
+  fi
 fi
 
 #run nextflow nextmb workflow ($1 is useful if you want to run resume)

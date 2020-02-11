59ec20352b58c2253a7f71c1d7a57548ab87302b
NOEL <cyril.noel@ifremer.fr>
2020-02-11 10:26:50 +0100

    Update create_phyloseq_obj.R

--- lib/create_phyloseq_obj.R
+++ lib/create_phyloseq_obj.R
@@ -21,14 +21,14 @@
 ## under the terms of the GNU Affero General Public License as published by  ##
 ## the Free Software Foundation, either version 3 of the License, or         ##
 ## (at your option) any later version.                                       ## 
-##									     ##
-## License at https://www.gnu.org/licenses/agpl-3.0.txt 		     ##
-## 									     ##
+##                                                                           ##
+## License at https://www.gnu.org/licenses/agpl-3.0.txt                      ##
+##                                                                           ##
 ## This program is distributed in the hope that it will be useful, but       ##
 ## WITHOUT ANY WARRANTY; without even the implied warranty of                ##
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                      ##
 ## See the GNU Affero General Public License for more details.               ##
-##									     ##
+##                                                                           ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ##                                                                           ##
 ## Notes: This part of the script performs only the creation of a phyloseq   ##

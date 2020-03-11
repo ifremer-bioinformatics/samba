a4341316a8bd6d9f9b2c3e5906b4c9d612bdec35
Cyril Noël <cyril.noel@ifremer.fr>
2020-03-11 16:42:43 +0000

    Update

--- lib/q2_picrust2.sh
+++ lib/q2_picrust2.sh
@@ -71,21 +71,21 @@ eval $cmd
 
 #Add descriptions EC
 cmd="sed -i '1d' $output/ec_metagenome_exported/ec_metagenome_predictions.tsv ;
-sed -i 's/#OTU ID	/EC_ID	/g' $output/ec_metagenome_exported/ec_metagenome_predictions.tsv ;
+sed -i 's/#OTU ID/EC_ID/g' $output/ec_metagenome_exported/ec_metagenome_predictions.tsv ;
 add_descriptions.py -i $output/ec_metagenome_exported/ec_metagenome_predictions.tsv -m EC -o $output/ec_metagenome_exported/ec_metagenome_predictions_with-descriptions.tsv "
 echo $cmd >> $logcmd
 eval $cmd
 
 #Add descriptions KO
 cmd="sed -i '1d' $output/ko_metagenome_exported/ko_metagenome_predictions.tsv ;
-sed -i 's/#OTU ID	/KO_ID	/g' $output/ko_metagenome_exported/ko_metagenome_predictions.tsv ;
+sed -i 's/#OTU ID/KO_ID/g' $output/ko_metagenome_exported/ko_metagenome_predictions.tsv ;
 add_descriptions.py -i $output/ko_metagenome_exported/ko_metagenome_predictions.tsv -m KO -o $output/ko_metagenome_exported/ko_metagenome_predictions_with-descriptions.tsv "
 echo $cmd >> $logcmd
 eval $cmd
 
 #Add descriptions METACYC
 cmd="sed -i '1d' $output/pathway_abundance_exported/pathway_abundance_predictions.tsv ;
-sed -i 's/#OTU ID	/METACYC_ID	/g' $output/pathway_abundance_exported/pathway_abundance_predictions.tsv ;
+sed -i 's/#OTU ID/METACYC_ID/g' $output/pathway_abundance_exported/pathway_abundance_predictions.tsv ;
 add_descriptions.py -i $output/pathway_abundance_exported/pathway_abundance_predictions.tsv -m METACYC -o $output/pathway_abundance_exported/pathway_abundance_predictions_with-descriptions.tsv"
 echo $cmd >> $logcmd
 eval $cmd

4dc5e5ae831c2289c9751c8ce1135ed305d09fdc
Cyril Noël <cyril.noel@ifremer.fr>
2020-03-12 08:27:05 +0000

    functional predictions ordination

---
+++ lib/functional_predictions.R
@@ -0,0 +1,142 @@
+#!/usr/bin/env Rscript
+###############################################################################
+##                                                                           ##
+## Script name: R script for the SAMBA-nextflow workflow                   ####
+##                                                                           ##
+## Purpose of script: Statistical representation of functional predictions   ##
+##                                                                           ##
+##                                                                           ##
+##                                                                           ##
+## Authors: Laure QUINTRIC and Cyril NOEL                                  ####
+##          Bioinformatics engineers                                         ##
+##          SeBiMER, Ifremer                                                 ##
+##                                                                           ##
+## Creation Date: 2020-03-11                                               ####
+## Modified on: 2020-03-11                                                 ####
+##                                                                           ##
+## Emails: samba-sebimer@ifremer.fr                                        ####
+##                                                                           ##
+## Copyright (c) SeBiMER, march-2020                                      ####
+## This program is free software: you can redistribute it and/or modify it   ##
+## under the terms of the GNU Affero General Public License as published by  ##
+## the Free Software Foundation, either version 3 of the License, or         ##
+## (at your option) any later version.                                       ## 
+##                                                                           ##
+## License at https://www.gnu.org/licenses/agpl-3.0.txt                      ##
+##                                                                           ##
+## This program is distributed in the hope that it will be useful, but       ##
+## WITHOUT ANY WARRANTY; without even the implied warranty of                ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                      ##
+## See the GNU Affero General Public License for more details.               ##
+##                                                                           ##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+##                                                                           ##
+## Notes: This script allows the nmds representation of the functional       ##
+##        predictions from metabarcoding data                                ##
+##                                                                           ##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+## Load up the needed packages ####
+requiredPackages = c("dplyr","ggplot2","RColorBrewer","svglite","vegan")
+for(package in requiredPackages){
+  library(package,character.only = TRUE)
+}
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
+#                                                                               #
+# Function to standardize ordination analysis of the functional predictions     #
+#                                                                               #
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
+
+functional_predictions <- function(pred,metadata,criteria,pred_plot,name) {
+  
+  # Format functional predictions tables ####
+  pred = read.table(pred,h=T,sep="\t",check.names=F,quote="")
+  pred = pred[,c(1,3:length(pred))]
+  rownames(pred) = pred[,1] 
+  pred = pred %>% select (-1)
+  t_pred = as.data.frame(t(pred))
+  t_pred = round(t_pred)
+  
+  # Load metatada
+  metadata = read.table(metadata, h=T, sep="\t", check.names=F)
+  
+  # Perform normalization
+  min_depth = min(colSums(pred))
+  t_pred_norm = as.data.frame(round(rrarefy(t_pred, min_depth)))
+  
+  # Calculate the distance matrix
+  pred_dist = as.matrix((vegdist(t_pred_norm, "bray")))
+  
+  # Perform NMDS
+  pred_nmds = metaMDS(pred_dist, k=2, trymax=100)
+  pred_nmds_stress = round(pred_nmds$stress,3)
+  
+  # Build a data frame with NMDS coordinates and metadata
+  pred_nmds1 = pred_nmds$points[,1]
+  pred_nmds2 = pred_nmds$points[,2]
+  pred_nmds_data = data.frame(MDS1 = pred_nmds1, MDS2 = pred_nmds2, metadata = metadata[,criteria])
+  print(pred_nmds_data)
+  
+  # ANOSIM statistic
+  pred_adonis = adonis(pred_dist ~ metadata[,criteria])
+  
+  # Plot
+  ggplot(pred_nmds_data, aes(x=MDS1, y=MDS2, col=metadata)) + 
+    theme_classic() +
+    geom_point(shape=19, size=3) +
+    stat_ellipse(geom="polygon",alpha=0.1,type="t",aes(fill=metadata),lty=2) +
+    scale_color_brewer(palette="Set1") +
+    theme(legend.title = element_blank()) +
+    theme(axis.text = element_text(colour = "black", size = 10)) +
+    labs(caption = paste("Stress:",pred_nmds_stress,
+                         "\nAdonis statistic R:",round(pred_adonis$aov.tab$R2[1]*100,2),
+                         paste("\nAdonis based on ", "transect_name",": p-value"),pred_adonis$aov.tab$`Pr(>F)`[1],sep=" "))
+  ggsave(filename=paste(name,pred_plot,".svg",sep=""), device="svg", width = 12, height = 10)
+  ggsave(filename=paste(name,pred_plot,".png",sep=""), device="png", width = 12, height = 10)
+}
+
+main_ec <- function(){
+  # Get arguments from RScript command line
+  args = commandArgs(trailingOnly=TRUE)
+  pred_ec = args[1]
+  metadata =   args[4]
+  criteria = args[5]
+  pred_plot = args[6]
+  name = "EC_"
+  functional_predictions(pred_ec,metadata,criteria,pred_plot,name)
+}
+
+if (!interactive()) {
+  main_ec()
+}
+
+main_ko <- function(){
+  # Get arguments from RScript command line
+  args = commandArgs(trailingOnly=TRUE)
+  pred_ko = args[2]
+  metadata = args[4]
+  criteria = args[5]
+  pred_plot = args[6]
+  name = "_KO"
+  functional_predictions(pred_ko,metadata,criteria,pred_plot,name)
+}
+
+if (!interactive()) {
+  main_ko()
+}
+
+main_metacyc <- function(){
+  # Get arguments from RScript command line
+  args = commandArgs(trailingOnly=TRUE)
+  pred_path = args[3]
+  metadata = args[4]
+  criteria = args[5]
+  pred_plot = args[6]
+  name = "_MetaCyc"
+  functional_predictions(pred_path,metadata,criteria,pred_plot,name)
+}
+
+if (!interactive()) {
+  main_metacyc()
+}

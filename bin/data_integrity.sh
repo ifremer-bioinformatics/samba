fd365030589fa3a3d20c81e5f9e4fb48d11653a6
Laure QUINTRIC <lquintri@datarmor1.ib0.ice.ifremer.fr>
2020-05-15 07:31:37 +0000

    change test in if clause to take in account R2 files

--- bin/data_integrity.sh
+++ bin/data_integrity.sh
@@ -46,9 +46,10 @@ COL=$(head -n1 ${manifest} | tr "\t" "\n" | grep -n ${R1_files} | cut -d ":" -f1
 cut -f$COL ${manifest} | sed '1d' > tmp_R1
 
 #get R2 file path from manifest file
-[ ! ${singleEnd} ] && COL=`head -n1 ${manifest} | tr "\t" "\n" | grep -n ${R2_files} | cut -d ":" -f1`
-[ ! ${singleEnd} ] && cut -f$COL ${manifest} | sed '1d' > tmp_R2
-
+if ! ${singleEnd}; then
+   COL=`head -n1 ${manifest} | tr "\t" "\n" | grep -n ${R2_files} | cut -d ":" -f1`
+   cut -f$COL ${manifest} | sed '1d' > tmp_R2
+fi
 #get barcode
 COL=$(head -n1 ${metadata} | tr "\t" "\n" | grep -n ${barcode} | cut -d ":" -f1)
 cut -f$COL ${metadata} | sed '1d' > tmp_barcodes
@@ -58,43 +59,95 @@ for i in $(cat tmp_R1) ; do zcat ${i} > reads; head -n 1 reads | cut -d ':' -f1 
 
 # Count and summary
 paste -d ' ' tmp_barcodes tmp_R1 | sed "s/^/zgrep -c ':N:0:/g" | sed "s/ /' /3" | sh > tmp_barcodes_count_R1
-[ ! ${singleEnd} ] && paste -d ' ' tmp_barcodes tmp_R2 | sed "s/^/zgrep -c ':N:0:/g" | sed "s/ /' /3" | sh > tmp_barcodes_count_R2
+if ! ${singleEnd}; then
+   paste -d ' ' tmp_barcodes tmp_R2 | sed "s/^/zgrep -c ':N:0:/g" | sed "s/ /' /3" | sh > tmp_barcodes_count_R2
+fi
 paste -d ' ' tmp_sequencer tmp_R1 | sed "s/^/zgrep -c '/g" | sed "s/ /' /3" | sh > tmp_sequencer_count_R1
-[ ! ${singleEnd} ] && paste -d ' ' tmp_sequencer tmp_R2 | sed "s/^/zgrep -c '/g" | sed "s/ /' /3" | sh > tmp_sequencer_count_R2
+if ! ${singleEnd}; then
+   paste -d ' ' tmp_sequencer tmp_R2 | sed "s/^/zgrep -c '/g" | sed "s/ /' /3" | sh > tmp_sequencer_count_R2
+fi
+
 for i in $(cat tmp_R1) ; do zgrep -c "${primerF}" ${i} >>  tmp_primer_count_R1 ; done
-[ ! ${singleEnd} ] && for i in $(cat tmp_R2) ; do zgrep -c "${primerR}" ${i} >>  tmp_primer_count_R2 ; done
-[ ! ${singleEnd} ] && paste -d '      ' tmp_sampleID tmp_barcodes tmp_reads tmp_barcodes_count_R1 tmp_barcodes_count_R2 tmp_sequencer_count_R1 tmp_sequencer_count_R2 tmp_primer_count_R1 tmp_primer_count_R2 > tmp_output
-[ ${singleEnd} ] && paste -d '	' tmp_sampleID tmp_barcodes tmp_reads tmp_barcodes_count_R1 tmp_sequencer_count_R1 tmp_primer_count_R1 > tmp_output
+if ! ${singleEnd}; then
+   for i in $(cat tmp_R2) ; do zgrep -c "${primerR}" ${i} >>  tmp_primer_count_R2 ; done
+   paste -d '      ' tmp_sampleID tmp_barcodes tmp_reads tmp_barcodes_count_R1 tmp_barcodes_count_R2 tmp_sequencer_count_R1 tmp_sequencer_count_R2 tmp_primer_count_R1 tmp_primer_count_R2 > tmp_output
+else
+   paste -d '	' tmp_sampleID tmp_barcodes tmp_reads tmp_barcodes_count_R1 tmp_sequencer_count_R1 tmp_primer_count_R1 > tmp_output
+fi
 
 # Barcode verification
 valcol=4 #Count_in_R1
-[ ${singleEnd} ] && addcol=7 #column = Perc_correct_barecode_R1
-[ ! ${singleEnd} ] && addcol=10 #column = Perc_correct_barecode_R1
+if ${singleEnd}; then
+   addcol=7 #column = Perc_correct_barecode_R1
+else
+    addcol=10 #column = Perc_correct_barecode_R1
+fi
 awk -v OFS='\t' -v "newcol=${addcol}" -v "col=${valcol}" '{$newcol = ($col != 0) ? sprintf("%.0f", $col/$3*100) : "0"}1' tmp_output | sort -nr -k ${addcol} > tmp_output.tmp && mv tmp_output.tmp tmp_output
 if [ $(tail -n 1 tmp_output | awk -F "\t" -v "newcol=${addcol}" '{print $newcol}') -ge ${barcode_filter} ] ; then touch barcodes_R1.ok ; else touch barcodes_R1.bad ; fi
-[ ! ${singleEnd} ] && awk -v OFS='\t' -v "newcol=$(($addcol+1))" -v "col=$(($valcol+1))" '{$newcol = ($col != 0) ? sprintf("%.0f", $col/$3*100) : "0"}1' tmp_output | sort -nr -k $(($addcol+1)) > tmp_output.tmp && mv tmp_output.tmp tmp_output
-[ ! ${singleEnd} ] && if [ $(tail -n 1 tmp_output | awk -F"\t" -v "newcol=$(($addcol+1))" '{print $newcol}') -ge ${barcode_filter} ] ; then touch barcodes_R2.ok ; else touch barcodes_R2.bad ; fi
+if ! ${singleEnd}; then
+   awk -v OFS='\t' -v "newcol=$(($addcol+1))" -v "col=$(($valcol+1))" '{$newcol = ($col != 0) ? sprintf("%.0f", $col/$3*100) : "0"}1' tmp_output | sort -nr -k $(($addcol+1)) > tmp_output.tmp && mv tmp_output.tmp tmp_output
+   if [ $(tail -n 1 tmp_output | awk -F"\t" -v "newcol=$(($addcol+1))" '{print $newcol}') -ge ${barcode_filter} ] ; then 
+      touch barcodes_R2.ok 
+   else 
+      touch barcodes_R2.bad
+   fi
+fi
 
 # Unique sequencer verification (Sequencer_R1, Sequencer_R2)
-[ ${singleEnd} ] && valcol=5;addcol=8 #column = Perc_uniq_sequencer_R1 
-[ ! ${singleEnd} ] && valcol=6;addcol=12 #column = Perc_uniq_sequencer_R1
+if ${singleEnd}; then 
+   valcol=5;addcol=8 #column = Perc_uniq_sequencer_R1 
+else
+   valcol=6;addcol=12 #column = Perc_uniq_sequencer_R1
+fi
 awk -v OFS='\t' -v "newcol=${addcol}" -v "col=${valcol}" '{$newcol = ($col != 0) ? sprintf("%.0f", $col/$3*100) : "0"}1' tmp_output | sort -nr -k ${addcol} > tmp_output.tmp && mv tmp_output.tmp tmp_output
 if [ $(tail -n 1 tmp_output | awk -F "\t" -v "newcol=${addcol}" '{print $newcol}') -ge 100 ] ; then touch sequencer_R1.ok ; else touch sequencer_R1.bad ; fi
-[ ! ${singleEnd} ] && awk -v OFS='\t' -v "newcol=$(($addcol+1))" -v "col=$(($valcol+1))" '{$newcol = ($col != 0) ? sprintf("%.0f", $col/$3*100) : "0"}1' tmp_output | sort -nr -k $(($addcol+1)) > tmp_output.tmp && mv tmp_output.tmp tmp_output
-[ ! ${singleEnd} ] && if [ $(tail -n 1 tmp_output | awk -F "\t" -v "newcol=$(($addcol+1))" '{print $newcol}') -ge 100 ] ; then touch sequencer_R2.ok ; else touch sequencer_R2.bad ; fi
+if ! ${singleEnd}; then
+   awk -v OFS='\t' -v "newcol=$(($addcol+1))" -v "col=$(($valcol+1))" '{$newcol = ($col != 0) ? sprintf("%.0f", $col/$3*100) : "0"}1' tmp_output | sort -nr -k $(($addcol+1)) > tmp_output.tmp && mv tmp_output.tmp tmp_output
+   if [ $(tail -n 1 tmp_output | awk -F "\t" -v "newcol=$(($addcol+1))" '{print $newcol}') -ge 100 ] ; then
+      touch sequencer_R2.ok
+   else 
+      touch sequencer_R2.bad
+   fi
+fi
 
 # Primer verification (PrimerF_in_R1)
-[ ${singleEnd} ] && valcol=6;addcol=9 #column = Perc_primer_R1
-[ ! ${singleEnd} ] && valcol=8;addcol=14 #column = Perc_primer_R1
+if ${singleEnd}; then
+   valcol=6
+   addcol=9 #column = Perc_primer_R1
+else 
+   valcol=8
+   addcol=14 #column = Perc_primer_R1
+fi
 awk -v OFS='\t' -v "newcol=${addcol}" -v "col=${valcol}" '{$newcol = ($col != 0) ? sprintf("%.0f", $col/$3*100) : "0"}1' tmp_output | sort -nr -k ${addcol} > tmp_output.tmp && mv tmp_output.tmp tmp_output
 if [ $(tail -n 1 tmp_output | awk -F "\t" -v "newcol=${addcol}" '{print $newcol}') -ge ${primer_filter} ] ; then touch primer_R1.ok ; else touch primer_R1.bad ; fi
-[ ! ${singleEnd} ] && awk -v OFS='\t' -v "newcol=$(($addcol+1))" -v "col=$(($valcol+1))" '{$newcol = ($col != 0) ? sprintf("%.0f", $col/$3*100) : "0"}1' tmp_output | sort -nr -k $(($addcol+1)) > tmp_output.tmp && mv tmp_output.tmp tmp_output
-[ ! ${singleEnd} ] && if [ $(tail -n 1 tmp_output | awk -F "\t" -v "newcol=$(($addcol+1))" '{print $newcol}') -ge ${primer_filter} ] ; then touch primer_R2.ok ; else touch primer_R2.bad ; fi
+if ! ${singleEnd}; then
+   awk -v OFS='\t' -v "newcol=$(($addcol+1))" -v "col=$(($valcol+1))" '{$newcol = ($col != 0) ? sprintf("%.0f", $col/$3*100) : "0"}1' tmp_output | sort -nr -k $(($addcol+1)) > tmp_output.tmp && mv tmp_output.tmp tmp_output
+   if [ $(tail -n 1 tmp_output | awk -F "\t" -v "newcol=$(($addcol+1))" '{print $newcol}') -ge ${primer_filter} ] ; then 
+      touch primer_R2.ok
+   else 
+      touch primer_R2.bad
+   fi
+fi
 
 # Results saving and remove all temporary files
-[ ${singleEnd} ] && if [[ -e barcodes_R1.ok && -e sequencer_R1.ok && -e primer_R1.ok ]] ; then echo '@@@ --- Your data passed the control --- @@@' ; else touch ${verif_bad} && echo '@@@ --- All controls are not satisfied --- @@@' ; fi
-[ ! ${singleEnd} ] && if [[ -e barcodes_R1.ok && -e barcodes_R2.ok && -e sequencer_R1.ok && -e sequencer_R2.ok && -e primer_R1.ok && -e primer_R2.ok ]] ; then echo '@@@ --- Your data passed the control --- @@@' ; else touch ${verif_bad} && echo '@@@ --- All controls are not satisfied --- @@@' ; fi
+if ${singleEnd}; then
+   if [[ -e barcodes_R1.ok && -e sequencer_R1.ok && -e primer_R1.ok ]] ; then 
+      echo '@@@ --- Your data passed the control --- @@@'
+   else 
+      touch ${verif_bad} && echo '@@@ --- All controls are not satisfied --- @@@'
+   fi
+else
+   if [[ -e barcodes_R1.ok && -e barcodes_R2.ok && -e sequencer_R1.ok && -e sequencer_R2.ok && -e primer_R1.ok && -e primer_R2.ok ]] ; then 
+      echo '@@@ --- Your data passed the control --- @@@' 
+   else 
+      touch ${verif_bad} && echo '@@@ --- All controls are not satisfied --- @@@'
+   fi
+fi
 mv tmp_output ${summary}
-[ ${singleEnd} ] && sed -i '1 i\SampleID\tBarcode\tReads_count\tCount_in_R1\tSequencer_R1\tPrimerF_in_R1\tPerc_correct_barcode_R1\tPerc_uniq_sequencer_R1\tPerc_primerF_R1' ${summary}
-[ ! ${singleEnd} ] && sed -i '1 i\SampleID\tBarcode\tReads_count\tCount_in_R1\tCount_in_R2\tSequencer_R1\tSequencer_R2\tPrimerF_in_R1\tPrimerR_in_R2\tPerc_correct_barcode_R1\tPerc_correct_barcode_R2\tPerc_uniq_sequencer_R1\tPerc_uniq_sequencer_R2\tPerc_primerF_R1\tPerc_primerR_R2' ${summary}
+if ${singleEnd}; then
+   sed -i '1 i\SampleID\tBarcode\tReads_count\tCount_in_R1\tSequencer_R1\tPrimerF_in_R1\tPerc_correct_barcode_R1\tPerc_uniq_sequencer_R1\tPerc_primerF_R1' ${summary}
+else
+   sed -i '1 i\SampleID\tBarcode\tReads_count\tCount_in_R1\tCount_in_R2\tSequencer_R1\tSequencer_R2\tPrimerF_in_R1\tPrimerR_in_R2\tPerc_correct_barcode_R1\tPerc_correct_barcode_R2\tPerc_uniq_sequencer_R1\tPerc_uniq_sequencer_R2\tPerc_primerF_R1\tPerc_primerR_R2' ${summary}
+fi
+
 rm tmp*

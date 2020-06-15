0cef8046f8e2ebf3939462d7af56ecbdf7e19ab2
galaxy <galaxy@datarmor3.ib0.ice.ifremer.fr>
2020-06-15 15:03:05 +0000

    remove institute specific files

--- conf/conda_envs/delenv.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Deactivate nextflow conda environment
-conda deactivate
-# Remove condabin from PATH (Modify the path according to your local installation)
-PATH=$(echo "$PATH" | sed -e 's@/appli/anaconda/versions/.*/condabin:@@g')
--- conf/conda_envs/nextflow_env.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Source conda.sh file according to your local conda installation
-. /appli/anaconda/latest/etc/profile.d/conda.sh
-# Activate Nextflow environment according to your local installation
-conda activate /appli/conda-env/bioinfo/nextflow-20.01.0

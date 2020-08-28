be5a12511725f3fd7a730058972384847221161c
NOEL <cyril.noel@ifremer.fr>
2020-08-28 16:04:21 +0200

    Add control of null blanks samples

--- bin/microDecon.R
+++ bin/microDecon.R
@@ -28,8 +28,11 @@ output_ASVs_removed = args[7]
 
 # Load ASV table
 TABLE = read.table(ASV_table,h=T,sep="\t",check.names=FALSE)
+TABLE = TABLE[,colSums(TABLE != 0) >0]
+control_list = control_list[control_list %in% colnames(TABLE)]
 TABLE_control = TABLE %>% select("ASV_ID",c(control_list), everything())  
-head(TABLE_control)
+blanks = length(control_list)
+samples = (length(TABLE_control)-2)-blanks
 
 # microDecon
 result = decon(TABLE_control, numb.blanks=as.numeric(blanks), numb.ind=as.numeric(samples), taxa=TRUE, runs=2, thresh=1)

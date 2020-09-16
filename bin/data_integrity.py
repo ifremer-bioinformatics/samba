3c6b9e6e69520bcda5e568284273dcf1eb9079d5
Alexandre Cormier <alexandre.cormier@ifremer.fr>
2020-09-16 08:43:32 +0000

    add controls and prepare for manifest/metadata rewritting

--- bin/data_integrity.py
+++ bin/data_integrity.py
@@ -6,7 +6,6 @@ import gzip
 import argparse
 import sys
 from Bio import SeqIO
-# from joblib import Parallel, delayed
 import multiprocessing as mp
 
 # For errors / warnings
@@ -17,19 +16,22 @@ def getArgs():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument('-a',dest="manifest",type=str,required=True,help='q2_manifest file')
     parser.add_argument('-e',dest="metadata",type=str,required=True,help='q2_metadata file')
-    parser.add_argument('-c',dest="threads",type=int,default=4,help='Number of threads [%(default)s]')
-    parser.add_argument('-t',dest="type",type=str,default='paired', choices=['paired','single'],help='Sequencing format [%(default)s]')
+    parser.add_argument('-p',dest="primer",type=int,default=70,help='Percentage of primers supposed to be found in raw reads [%(default)s]')
+    parser.add_argument('-c',dest="control",type=str,help='Delimited list of control (comma separated)')
+    parser.add_argument('-r',dest="readtype",type=str,default='paired', choices=['paired','single'],help='Sequencing format [%(default)s]')
+    parser.add_argument('-t',dest="threads",type=int,default=4,help='Number of threads [%(default)s]')
 
     arg = parser.parse_args()
+
     return arg
 
 def collect_check_metadata(metadata_file, data_type):
     collect_data = {}
     # read metadata line by line
     for l in open(metadata_file, 'r'):
-        sampleid = l.split()[0]
         # ignore header
-        if sampleid != 'sampleid':
+        if not l.startswith('sampleid'):
+            sampleid = l.split()[0]
             collect_data[sampleid] = {}
             collect_data[sampleid]['barcode'] = l.split()[1]
             if data_type == 'paired':
@@ -42,27 +44,41 @@ def collect_check_metadata(metadata_file, data_type):
             # check that var(s) didn't contains any NA
             for var in collect_data[sampleid]['vars']:
                 if var == 'NA':
-                    eprint('ERROR: '+sampleid+' has NA value(s) in $metadata, please remove them before running SAMBA')
-                    verify_bad=open('verify.bad', 'w')
-                    verify_bad.close()
+                    eprint('ERROR: '+sampleid+' has NA value(s) in q2_metadata, please remove them before running SAMBA')
                     exit(1)
-    # process file validator
-    verify_ok=open('verify.ok', 'w')
-    verify_ok.close()
     # return results
     return collect_data, data_type
 
 def collect_manifest(manifest_file, collect_data, data_type):
+    # in order to check that metadata and manifest have the same number of lines
+    metadata_size = len(collect_data)
+    manifest_size = 0
+    # read manifest
     for l in open(manifest_file, 'r'):
         # ignore header
         if not l.startswith('sample-id'):
             if data_type == 'paired':
                 sampleid, R1, R2 = l.split()
-                collect_data[sampleid]['R1'] = R1
-                collect_data[sampleid]['R2'] = R2
+                try:
+                    collect_data[sampleid]['R1'] = R1
+                    collect_data[sampleid]['R2'] = R2
+                except KeyError:
+                    eprint('ERROR: '+sampleid+' from q2_manifest is absent in q2_metadata')
+                    exit(1)
             else:
                 sampleid, R1 = l.split()
-                collect_data[sampleid]['R1'] = R1
+                try:
+                    collect_data[sampleid]['R1'] = R1
+                except KeyError:
+                    eprint('ERROR: '+sampleid+' from q2_manifest is absent in q2_metadata')
+                    exit(1)
+            # increase line counter
+            manifest_size += 1
+    # check that manifest have the same size as metadata
+    if manifest_size != metadata_size:
+        eprint('ERROR: q2_manifest and q2_metadata did not have the same number of lines')
+        exit(1)
+    # return update dict
     return collect_data
 
 def check_fastq(collect_data, sample, data_type):
@@ -90,6 +106,7 @@ def check_fastq(collect_data, sample, data_type):
         collect_data[sample]['perc_primer_'+r] = round(primer * 100 / reads_count, 2)
         collect_data[sample]['nb_barcode_'+r] = len(index)
         collect_data[sample]['barcode_seq_'+r] = 'FALSE'
+        # chech that barcode are the same as in metadata
         if len(index) == 1:
             if index[0] == barcode:
                 collect_data[sample]['barcode_seq_'+r]  = 'TRUE'
@@ -139,10 +156,10 @@ def write_report(collect_data, data_type):
             report.write(sample+'\t'+'{reads_count_R1}\t{barcode}\t{nb_barcode_R1}\t{barcode_seq_R1}\t{nb_instrument_R1}\t{primer_R1}\t{perc_primer_R1}\n'.format(**val))
 
 def main(args):
-    
+
     # Collect metadata infos and check variable names
     print("Step 1 - parse and collect data from q2_metadata")
-    collect_data, data_type = collect_check_metadata(args.metadata, args.type)
+    collect_data, data_type = collect_check_metadata(args.metadata, args.readtype)
     # Collect reads location from manifest
     print("Step 2 - parse and collect data from q2_manifest")
     collect_data = collect_manifest(args.manifest, collect_data, data_type)
@@ -156,9 +173,37 @@ def main(args):
     for result in collect_data_para:
         for sample, vals in result.items():
             collect_data_out[sample] = vals
-    # Validate and report fastq intergrity
+    # Report fastq intergrity before validation
+    # Allow exploration by user for a better understanding
     print("Step 4 - write integrity report")
     write_report(collect_data_out, data_type)
+    # Validate manifest, metadata and fastq
+    print("Step 5 - validation of data")
+    controls = [str(control) for control in args.control.split(',')]
+    print(controls)
+    for sample, val in collect_data_out.items():
+        # print(sample, val)
+        # check samples
+        if val['reads_count_R1'] != val['reads_count_R2']:
+            eprint('ERROR: '+sampleid+' from q2_manifest is absent in q2_metadata')
+            exit(1)
+
+            
+        if not sample in controls:
+
+            # if val['nb_instrument_R1'] != val['reads_count_R2']:
+            if val['perc_primer_R1'] <= args.primer or val['perc_primer_R2'] <= args.primer:
+                eprint('ERROR: '+sampleid+' from q2_manifest is absent in q2_metadata')
+                exit(1)
+
+
+    # for k, v in collect_data_out.items()
+        # -> primer filter
+        # -> primer filter expcet for controls
+        # -> barcode filter: should be == TRUE
+        # -> sequencer name filter: should == 1
+
+    # print("Step 6 - rewrite manifest and metadata (to sort and remove space)")
 
 if __name__ == '__main__':
     args = getArgs()

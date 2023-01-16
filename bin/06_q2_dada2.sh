11d13c9ae3ecbd91491d92da227eb5efa7630fe4
Cyril Noël <cyril.noel@ifremer.fr>
2023-01-16 12:14:58 +0000

    Add param to the number of sequence used by the DADA2 learning error process

--- bin/06_q2_dada2.sh
+++ bin/06_q2_dada2.sh
@@ -16,18 +16,19 @@ R_TRUNC_LENGTH=${args[5]}
 TRUNC_Q=${args[6]}
 F_MAX_EE=${args[7]}
 R_MAX_EE=${args[8]}
-POOLING_METHOD=${args[9]}
-CHIMERA_METHOD=${args[10]}
-CPUS=${args[11]}
-REP_SEQS_QZA=${args[12]}
-TABLE_QZA=${args[13]}
-DADA2_STATS_QZA=${args[14]}
-DADA2_STATS_QZV=${args[15]}
-TABLE_QZV=${args[16]}
-METADATA=${args[17]}
-REP_SEQS_QZV=${args[18]}
-DADA2_OUTPUT=${args[19]}
-LOGCMD=${args[20]}
+N_READS_LEARN=${args[9]}
+POOLING_METHOD=${args[10]}
+CHIMERA_METHOD=${args[11]}
+CPUS=${args[12]}
+REP_SEQS_QZA=${args[13]}
+TABLE_QZA=${args[14]}
+DADA2_STATS_QZA=${args[15]}
+DADA2_STATS_QZV=${args[16]}
+TABLE_QZV=${args[17]}
+METADATA=${args[18]}
+REP_SEQS_QZV=${args[19]}
+DADA2_OUTPUT=${args[20]}
+LOGCMD=${args[21]}
 
 if ${SINGLE_END}
 then
@@ -37,7 +38,7 @@ else
 fi
 
 # Run DADA2
-CMD="${options} --verbose --i-demultiplexed-seqs ${DADA2_INPUT_DATA} --p-trunc-q ${TRUNC_Q} --p-pooling-method ${POOLING_METHOD} --p-chimera-method ${CHIMERA_METHOD} --p-n-threads ${CPUS} --o-representative-sequences ${REP_SEQS_QZA} --o-table ${TABLE_QZA} --o-denoising-stats ${DADA2_STATS_QZA}"
+CMD="${options} --verbose --i-demultiplexed-seqs ${DADA2_INPUT_DATA} --p-trunc-q ${TRUNC_Q} --p-n-reads-learn ${N_READS_LEARN} --p-pooling-method ${POOLING_METHOD} --p-chimera-method ${CHIMERA_METHOD} --p-n-threads ${CPUS} --o-representative-sequences ${REP_SEQS_QZA} --o-table ${TABLE_QZA} --o-denoising-stats ${DADA2_STATS_QZA}"
 echo ${CMD} > ${LOGCMD}
 eval ${CMD}
 
@@ -61,5 +62,30 @@ CMD="qiime tools export --input-path ${REP_SEQS_QZV} --output-path ${DADA2_OUTPU
 qiime tools export --input-path ${TABLE_QZA} --output-path ${DADA2_OUTPUT} ;
 qiime tools export --input-path ${TABLE_QZV} --output-path ${DADA2_OUTPUT} ;
 qiime tools export --input-path ${DADA2_STATS_QZV} --output-path ${DADA2_OUTPUT}"
+echo ${CMD} >> ${LOGCMD}
+eval ${CMD}
+
+# Convert BIOM table to TSV
+CMD="biom convert -i ${DADA2_OUTPUT}/feature-table.biom -o ${DADA2_OUTPUT}/dada2_asv_table.tsv --to-tsv ;
+sed -i '1d' ${DADA2_OUTPUT}/dada2_asv_table.tsv ;
+sed -i 's/#OTU ID/ASV_ID/g' ${DADA2_OUTPUT}/dada2_asv_table.tsv"
+echo ${CMD} >> ${LOGCMD}
+eval ${CMD}
+
+# Associate abundance to each ASV sequence
+# -> for further header modification of the ASV FASTA file
+CMD="sed '1d' ${DADA2_OUTPUT}/feature-frequency-detail.csv > asv_abundance_header.txt ;
+sed -i 's/,/_/g' asv_abundance_header.txt ;
+sed -i 's/.0$//g' asv_abundance_header.txt ;
+cut -d '_' -f1 asv_abundance_header.txt > asv_id.txt"
+echo ${CMD} >> ${LOGCMD}
+eval ${CMD}
+
+# Modify the ASV sequence header of the ASV FASTA file
+CMD="cp ${DADA2_OUTPUT}/sequences.fasta sequences_with_abundance.fasta ;
+sed 's/^/sed -i \"s\//g' asv_id.txt > tmp_rename_ASV_sequence_header.sh ;
+paste -d '\/' tmp_rename_ASV_sequence_header.sh asv_abundance_header.txt > rename_ASV_sequence_header.sh ;
+sed -i 's/$/\/g\" sequences_with_abundance.fasta/g' rename_ASV_sequence_header.sh ;
+sh rename_ASV_sequence_header.sh"
 echo ${CMD} >> ${LOGCMD}
 eval ${CMD}

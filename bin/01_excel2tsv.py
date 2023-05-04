576842a45afd10a84670fd3d22fa004788f40f99
Alexandre Cormier <cormieralexandre@gmail.com>
2023-05-04 10:31:15 +0200

    [bugfix] Nanopore/Pacbio not have primer/barcode

--- bin/01_excel2tsv.py
+++ bin/01_excel2tsv.py
@@ -12,9 +12,11 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option('--xls', '-x', type=click.Path(exists=True), required=True,
               help='Excel file for manifest & metadata in XLS format. XLSX format is not supported.')
+@click.option('--datatype', '-d', type=click.Choice(['illumina', 'nanopore', 'pacbio']), default='illumina',
+              help='Sequencing type [illumina]')
 @click.option('--seqtype', '-s', type=click.Choice(['single', 'paired']), default='paired',
               help='Sequencing type [paired]')
-def main(xls, seqtype):
+def main(xls, datatype, seqtype):
     """Control and convert Excel input as metadata.tsv and manifest.tsv"""
     data_extraction = {}
     # 1 - Load data
@@ -24,13 +26,13 @@ def main(xls, seqtype):
     logger.info(f'Start to validate XLS')
     check_samples(manifest, metadata)
     check_seqtype(manifest, seqtype)
-    check_metadata(metadata, seqtype)
+    check_metadata(metadata, datatype, seqtype)
     logger.success(f'Successfully validate XLS')
     # 3 - Export XLS to TSV for Qiime2
     logger.info(f'Start to export XLS to TSV')
     data_extraction = extract_manifest(manifest, seqtype, data_extraction)
-    data_extraction, metadata_vars = extract_metadata(metadata, seqtype, data_extraction)
-    export_to_tsv_for_qiime(data_extraction, metadata_vars, seqtype)
+    data_extraction, metadata_vars = extract_metadata(metadata, datatype, seqtype, data_extraction)
+    export_to_tsv_for_qiime(data_extraction, metadata_vars, datatype, seqtype)
     logger.success(f'Done')
 
 
@@ -91,16 +93,18 @@ def check_seqtype(manifest, seqtype):
         logger.success(f'Manifest is consistent with sequence type (SE/PE)')
 
 
-def check_metadata(metadata, seqtype):
+def check_metadata(metadata, datatype, seqtype):
     logger.info(f'Check metadata formatting')
     row_count_metadata = metadata.nrows
     for row in range(1, row_count_metadata):
         tmp = metadata.row_values(row, start_colx=0, end_colx=None)
         sample = tmp[0].strip(' ')
-        if seqtype == 'single':
-            meta_var = [x.strip(' ') for x in map(str, tmp[3:])]
-        else:
-            meta_var = [x.strip(' ') for x in map(str, tmp[4:])]
+        start = 1
+        if datatype == 'illumina':
+            start = 4
+            if seqtype == 'single':
+                start = 3
+        meta_var = [x.strip(' ') for x in map(str, tmp[start:])]
         for var in meta_var:
             if var in ['NA', '', 'Nan', 'NaN', 'nan']:
                 logger.error(f'{sample} has unset values (NA, NaN, empty cell...) in metadata')
@@ -126,22 +130,25 @@ def extract_manifest(manifest, seqtype, data_extraction):
     return data_extraction
 
 
-def extract_metadata(metadata, seqtype, data_extraction):
+def extract_metadata(metadata, datatype, seqtype, data_extraction):
     logger.info(f'Extract metadata')
     row_count_metadata = metadata.nrows
     metadata_vars = []
     for row in range(1, row_count_metadata):
         tmp = metadata.row_values(row, start_colx=0, end_colx=None)
         sample = tmp[0].strip(' ')
-        data_extraction[sample]['barcode'] = tmp[1].strip(' ')
-        data_extraction[sample]['primerF'] = tmp[2].strip(' ')
-        if seqtype == 'single':
-            metadata_vars = [x.strip(' ') for x in map(str, metadata.row_values(0, start_colx=3, end_colx=None))]
-            data_extraction[sample]['vars'] = [x.strip(' ') for x in map(str, tmp[3:])]
-        else:
-            metadata_vars = [x.strip(' ') for x in map(str, metadata.row_values(0, start_colx=4, end_colx=None))]
-            data_extraction[sample]['primerR'] = tmp[3].strip(' ')
-            data_extraction[sample]['vars'] = [x.strip(' ') for x in map(str, tmp[4:])]
+        start = 1
+        if datatype == 'illumina':
+            start = 3
+            data_extraction[sample]['barcode'] = tmp[1].strip(' ')
+            data_extraction[sample]['primerF'] = tmp[2].strip(' ')
+            if seqtype == 'paired':
+                start = 4
+                data_extraction[sample]['primerR'] = tmp[3].strip(' ')
+
+        metadata_vars = [x.strip(' ') for x in map(str, metadata.row_values(0, start_colx=start, end_colx=None))]
+        data_extraction[sample]['vars'] = [x.strip(' ') for x in map(str, tmp[start:])]
+
         for var in data_extraction[sample]['vars']:
             if var in ['NA', '', 'Nan', 'NaN', 'nan']:
                 logger.error(f'{sample} has unset values (NA, NaN, empty cell...) in metadata')
@@ -151,31 +158,39 @@ def extract_metadata(metadata, seqtype, data_extraction):
     return data_extraction, metadata_vars
 
 
-def export_to_tsv_for_qiime(data, metadata_vars, seqtype):
+def export_to_tsv_for_qiime(data, metadata_vars, datatype, seqtype):
     logger.info(f'Start exporting XLSX to TSV for QIIME 2')
 
     manifest = open('q2_manifest.sort.tsv', 'w')
     metadata = open('q2_metadata.sort.tsv', 'w')
 
-    p_manifest = ['sample-id', 'forward-absolute-filepath', 'reverse-absolute-filepath']
-    s_manifest = ['sample-id', 'absolute-filepath']
-    p_metadata = ['sampleid', 'barcode', 'PrimerF', 'PrimerR']
-    s_metadata = ['sampleid', 'barcode', 'PrimerF']
+    h_manifest = ['sample-id', 'absolute-filepath']
+    if datatype == 'illumina' and seqtype == 'paired':
+        h_manifest.append('reverse-absolute-filepath')
+
+    h_metadata = ['sampleid']
+    if datatype == 'illumina':
+        h_metadata = h_metadata + ['barcode', 'PrimerF']
+        if seqtype == 'paired':
+            h_metadata.append('PrimerR')
 
     tab = '\t'
-    if seqtype == 'single':
-        manifest.write(f"{tab.join(s_manifest)}\n")
-        metadata.write(f"{tab.join(s_metadata)}\t{tab.join(metadata_vars)}\n")
-    else:
-        manifest.write(f"{tab.join(p_manifest)}\n")
-        metadata.write(f"{tab.join(p_metadata)}\t{tab.join(metadata_vars)}\n")
+    manifest.write(f"{tab.join(h_manifest)}\n")
+    metadata.write(f"{tab.join(h_metadata)}\t{tab.join(metadata_vars)}\n")
+
     for sample, values in data.items():
-        if seqtype == 'single':
-            manifest.write(f"{sample}\t{values['R1']}\n")
-            metadata.write(f"{sample}\t{values['barcode']}\t{values['primerF']}\t{tab.join(map(str, values['vars']))}\n")
-        else:
-            manifest.write(f"{sample}\t{values['R1']}\t{values['R2']}\n")
-            metadata.write(f"{sample}\t{values['barcode']}\t{values['primerF']}\t{values['primerR']}\t{tab.join(map(str, values['vars']))}\n")
+        l_manifest = [sample, values['R1']]
+        if datatype == 'illumina' and seqtype == 'paired':
+            l_manifest.append(values['R2'])
+        manifest.write(f"{tab.join(l_manifest)}\n")
+
+        l_metadata = [sample]
+        if datatype == 'illumina':
+            l_metadata = l_metadata + [values['barcode'], values['primerF']]
+            if seqtype == 'paired':
+                l_metadata.append(values['primerR'])
+        metadata.write(f"{tab.join(l_metadata)}\t{tab.join(map(str, values['vars']))}\n")
+
     manifest.close()
     metadata.close()
     logger.success(f'Successfully written to TSV')
